1b5c2fe5c23267b9d60abe8cc7ed68c5ed4767ac
matimorcz <mmorcos@ztabilize.com>
2024-11-08 15:18:06 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#e91e63',
+        'primary-foreground': '#ffffff',
+      },
+    },
   },
   plugins: [],
 }
